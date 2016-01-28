5992d27653a221fb4665e56b9725499505a336ce
Ray <rayrohder@gmail.com>
2016-01-28 20:40:31 +0100

    added logo's and txt files

--- ExampleAppComm2/ExampleAppComm2/MainPage.xaml.cs
+++ ExampleAppComm2/ExampleAppComm2/MainPage.xaml.cs
@@ -1,21 +1,17 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.AppService;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.ApplicationModel;
+using Windows.UI.Xaml.Media.Imaging;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,25 +27,72 @@ namespace ExampleAppComm2
             this.InitializeComponent();
 
 
-            textBlock.Text = "This is an example";
+            //textBlock.Text = "This is an example";
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            String con = "default";
+
+            var uri = new Uri("ms-appx:///Assets/logos/logo.png");
+
             try {
                 var arguments = e.Parameter as ProtocolActivatedEventArgs;
                 var set = arguments.Data as ValueSet;
-
-                textBlock.Text = set.ElementAt(0).Value.ToString();
+                con = set.ElementAt(0).Value.ToString();
+                //textBlock.Text = set.ElementAt(0).Value.ToString();
                 base.OnNavigatedTo(e);
             }
             catch(Exception ex)
             {
 
             }
+
+            switch (con)
+            {
+                case "Comiccon":
+                    uri = new Uri("ms-appx:///Assets/logos/dcc.png");
+                    break;
+                case "MGC Experience":
+                    uri = new Uri("ms-appx:///Assets/logos/mgc.jpg");
+                    break;
+                case "Tsunacon":
+                    uri = new Uri("ms-appx:///Assets/logos/tsunacon.png");
+                    break;
+                case "Nishicon":
+                    uri = new Uri("ms-appx:///Assets/logos/nishicon.png");
+                    break;
+                case "Tomocon":
+                    uri = new Uri("ms-appx:///Assets/logos/tomocon.jpg");
+                    break;
+                case "Animecon":
+                    uri = new Uri("ms-appx:///Assets/logos/animecon.jpg");
+                    break;
+            }
+
+            var bitmap = new BitmapImage(uri);
+            image.Source = bitmap;
+            textBlock.Text = getText(con);
         }
 
- 
+
+
+        private String getText(string con)
+        {
+            string text = "";
+            string filepath = @"Assets/info/"+con+".txt";
+
+            string[] myLines = File.ReadAllLines(filepath);
+            
+            for(int i = 0; i<myLines.Length; i++)
+            {
+                text += "\n" + myLines[i];
+            }
+
+            return text;
+        }
+
+
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
